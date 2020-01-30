3e6b2e5715798ca7f9a7a2f9bbb99d46282423e5
Bogdan Muţiu <bmutziu@gmail.com>
2020-01-30 20:55:39 +0200

    from onlineman477 to bmutziu

--- HR-API/test/features/support/init.js
+++ HR-API/test/features/support/init.js
@@ -7,7 +7,7 @@ const {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Before}) {
     Before(function() {
-        this.apickli = new apickli.Apickli('https', 'onlineman477-eval-prod.apigee.net');
+        this.apickli = new apickli.Apickli('https', 'bmutziu-eval-prod.apigee.net');
         this.apickli.addRequestHeader('Cache-Control', 'no-cache');
     });
 });
